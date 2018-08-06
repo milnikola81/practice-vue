6c6607ea5033ccfa10d808697ea2df52015ba289
Nikola <milnikola81@gmail.com>
2018-08-06 20:38:36 +0200

    finished no.3

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
       <h1>Contact List</h1>
+      <form @submit.prevent>
+          <label>Name</label>
+          <input v-model="newContact.name" type="text" placeholder="name"> <br>
+          <label>Email</label>
+          <input v-model="newContact.email" type="text" placeholder="email"> <br>
+          <button @click="addContact" type="submit">Add contact</button>
+      </form>
       <table>
           <thead>
               <th>Name</th>
@@ -26,8 +33,17 @@ export default {
               {name: "Jack", email: "jack@gmail.com"},
               {name: "Bob", email: "bob@gmail.com"},
               {name: "Bill", email: "bill@gmail.com"}
-          ]
+          ],
+          newContact: {
+              name: '',
+              email: ''
+          }
       }
+  },
+  methods: {
+        addContact() {
+            this.contacts.push(this.newContact);
+        }
   },
   props: {
     msg: String
